b1fd94675461c53325520781b39f4f9f4b11c572
aimanabolotbekova <bolotbekova_a@auca.kg>
2021-12-20 19:09:26 +0600

    1.7

---
+++ app/src/main/java/kg/test/individual/MainActivityInfo.kt
@@ -0,0 +1,11 @@
+package kg.test.individual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
