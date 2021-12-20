c90c312f19a98a9da745b3020e4d00c147f0bcb5
aimanabolotbekova <bolotbekova_a@auca.kg>
2021-12-20 09:09:31 +0600

    1.3

---
+++ app/src/main/java/kg/test/individual/MainActivityRegister.kt
@@ -0,0 +1,11 @@
+package kg.test.individual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`activity_register.xml`)
+    }
+}
